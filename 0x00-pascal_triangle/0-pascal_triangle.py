7caf2b2a88af5139bf065a6290000929cf71221a
Ntsako Ngoveni <naction923@gmail.com>
2023-12-06 11:16:50 -0800

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
-    # Check if n is less than or equal to 0
+    """
+         returns a list of lists of
+         integers representing
+          the Pascal’s triangle of n
+         Returns an empty list if n <= 0
+    """
     if n <= 0:
         return []
-
-    # Initialize the triangle with the first row
     triangle = [[1]]
-
-    # Generate the subsequent rows using zip
-    for _ in range(1, n):
-        # Use zip to combine the current row and its shifted version
-        row = [1] + [a + b for a, b in zip(triangle[-1], triangle[-1][1:])] + [1]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
         triangle.append(row)
-
     return triangle
